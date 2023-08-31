aa876ca18d59436d48fc9f5d91c3b3d8f4f1ca2f
mackingsley <kingsleyudoma2018@gmail.com>
2023-08-31 19:33:08 +0100

    4

--- controllers/wallets/balance.js
+++ controllers/wallets/balance.js
@@ -1,37 +1,38 @@
 const express = require("express");
-const Web3 = require("web3");
+//const Web3 = require("web3");
+const { Web3 } = require('web3');
 const dotenv = require("dotenv").config();
 const axios = require("axios");
 const {fromWei} = require('web3-utils');
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA));
 
 
 
-exports.getbalance =  async (req,res)=>{
 
-    const web3 = new Web3(process.env.INFURA);
+exports.getbalance = async (req, res) => {
     const address = req.params.wallet;
 
     try {
         const Bal = await web3.eth.getBalance(address);
         const rates = await axios.get(process.env.BALURL);
 
+        const ethBalance = parseFloat(web3.utils.fromWei(Bal, 'ether'));
+        const dollarBalance = ethBalance * rates.data.ethereum.usd;
+
         res.status(200).json({
-            message:"Success",
-            EthBalance:Bal,
-            DollarBalance:Bal*rates.data.ethereum.usd,
-            rates:rates.data.ethereum.usd
-
+            message: "Success",
+            EthBalance: ethBalance.toFixed(2), // Convert to string with 2 decimal places
+            DollarBalance: dollarBalance.toFixed(2), // Convert to string with 2 decimal places
+            rates: rates.data.ethereum.usd
         });
-        console.log(Bal);
 
+        console.log(ethBalance, dollarBalance);
     } catch (error) {
-        
         res.status(500).json({
-            message:"Error",
-            error:error
+            message: "Error",
+            error: error
         });
-       console.log(error);
+        console.log(error);
     }
-
-}
+};
 
