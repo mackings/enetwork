9e08a0cff83752681dd37dea96239ffe57eb20ab
mackingsley <kingsleyudoma2018@gmail.com>
2023-09-24 09:22:11 +0100

    sce

--- controllers/emailservice.js
+++ controllers/emailservice.js
@@ -52,53 +52,61 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 class OTPService {
   constructor() {
-    this.otpSecret = speakeasy.generateSecret().base32;
+    this.otpSecret = speakeasy.generateSecret({ length: 10 }).base32;
     this.transporter = nodemailer.createTransport({
       service: 'Gmail',
       auth: {
         user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD, 
-
+        pass: process.env.GMAIL_PASSWORD,
       },
     });
   }
 
   async sendOTP(email) {
-    const otp = speakeasy.totp({
-      secret: this.otpSecret,
-      step: 600,
-    });
-
-    const mailOptions = {
-      from: 'Macsonline500@gmail.com', // Your Gmail email address
-      to: email,
-      subject: 'Verification Codes',
-      text: `Your OTP code is: ${otp}`,
-    };
-
     try {
+      const otp = speakeasy.totp({
+        secret: this.otpSecret,
+        step: 600,
+      });
+  
+      const mailOptions = {
+        from: 'Macsonline500@gmail.com', // Your Gmail email address
+        to: email,
+        subject: 'Verification Code',
+        text: `Your OTP code is: ${otp}`,
+      };
+  
       const info = await this.transporter.sendMail(mailOptions);
       console.log('OTP sent: ' + info.response);
       console.log('Generated OTP:', otp);
+      console.log(this.otpSecret);
       return true;
     } catch (error) {
-      console.error(error.message);
+      console.error('Error sending OTP email:', error);
       return false;
     }
   }
+  
 
   verifyOTP(userOTP) {
-    const isValid = speakeasy.totp.verify({
+    const otpDetails = {
       secret: this.otpSecret,
       encoding: 'base32',
       token: userOTP,
-      window: 1,
-    });
-
-    console.log('Is Valid OTP?', isValid);
-
-    return isValid;
+      window: 3,
+    };
+  
+    try {
+      const isValid = speakeasy.totp.verify(otpDetails);
+      console.log('Is Valid OTP?', isValid);
+      console.log('OTP Details:', otpDetails);
+      return isValid;
+    } catch (error) {
+      console.error('Error during OTP verification:', error);
+      return false;
+    }
   }
+  
 }
 
 module.exports = OTPService;
