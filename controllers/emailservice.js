59d1bcadbceb1175ac3a890cd209dfb8705e40b8
mackingsley <kingsleyudoma2018@gmail.com>
2023-09-24 09:51:57 +0100

    otpfixed

--- controllers/emailservice.js
+++ controllers/emailservice.js
@@ -6,50 +6,6 @@ const nodemailer = require('nodemailer');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
-// class OTPService {
-//   constructor() {
-//     this.otpSecret = speakeasy.generateSecret().base32;
-//   }
-
-//   async sendOTP(email) {
-//     const otp = speakeasy.totp({
-//       secret: this.otpSecret,
-//       step: 300,
-//     });
-
-//     const msg = {
-//       to: email,
-//       from: 'Macsonline500@gmail', 
-//       subject: '<<C21B4D44-9C94-48C5-920B-2BFFF7705058>>',
-//       text: `Mail from Macsonline500@gmail.com on Sendgrid`,
-//      // text: `Your OTP code is: ${otp}`,
-//     };
-
-//     try {
-//       const info = await sgMail.send(msg);
-//       console.log('OTP sent: ' + info.response);
-//       return true;
-//     } catch (error) {
-//       console.error(error.message);
-//       return false;
-//     }
-//   }
-
-//   verifyOTP(userOTP) {
-//     const isValid = speakeasy.totp.verify({
-//       secret: this.otpSecret,
-//       encoding: 'base32',
-//       token: userOTP,
-//       window: 1,
-//     });
-
-//     return isValid;
-//   }
-// }
-
-// module.exports = OTPService;
-
-
 class OTPService {
   constructor() {
     this.otpSecret = speakeasy.generateSecret({ length: 10 }).base32;
@@ -64,13 +20,15 @@ class OTPService {
 
   async sendOTP(email) {
     try {
-      const otp = speakeasy.totp({
+      const otp = speakeasy.time({
         secret: this.otpSecret,
         step: 600,
+        encoding:"base32",
+        window:0
       });
   
       const mailOptions = {
-        from: 'Macsonline500@gmail.com', // Your Gmail email address
+        from: 'Macsonline500@gmail.com', 
         to: email,
         subject: 'Verification Code',
         text: `Your OTP code is: ${otp}`,
@@ -93,16 +51,19 @@ class OTPService {
       secret: this.otpSecret,
       encoding: 'base32',
       token: userOTP,
-      window: 3,
+      window: 0,
+      step:600
     };
   
     try {
-      const isValid = speakeasy.totp.verify(otpDetails);
+      const isValid = speakeasy.time.verify(otpDetails);
       console.log('Is Valid OTP?', isValid);
       console.log('OTP Details:', otpDetails);
+      console.log(isValid);
       return isValid;
     } catch (error) {
       console.error('Error during OTP verification:', error);
+      console.log(Date);
       return false;
     }
   }
